46f6dd5a8b18997f152cdc2d0de2e7baa9f8f8db
Anh Vo <139440902+vanh2753@users.noreply.github.com>
2025-08-18 16:34:55 +0700

    Update db-config.js

--- src/configs/db-config.js
+++ src/configs/db-config.js
@@ -5,6 +5,11 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
+     dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true
+      }
+    },
     logging: false
 });
 
