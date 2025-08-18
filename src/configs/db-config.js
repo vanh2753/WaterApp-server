2eb60441f6e940ecd10da6635dfa67f7812153fc
Anh Vo <139440902+vanh2753@users.noreply.github.com>
2025-08-18 16:44:31 +0700

    add db url

--- src/configs/db-config.js
+++ src/configs/db-config.js
@@ -1,17 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
-    dialect: 'mysql',
-     dialectOptions: {
-      ssl: {
-         require: true,              // bắt buộc dùng SSL
-        rejectUnauthorized: false   // bỏ qua self-signed cert
-      }
-    },
-    logging: false
+// Kết nối bằng URL
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,            // Clever Cloud yêu cầu SSL
+      rejectUnauthorized: false // Bỏ qua self-signed cert
+    }
+  },
+  logging: false,
 });
 
 module.exports = sequelize;
